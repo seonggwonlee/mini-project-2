8cb3838211dad3659a938c9dc6b0da954c95d1ae
seoggwonlee <sysktho@gmail.com>
2020-10-22 19:52:31 +0900

    help커맨드 수정

--- mini-project-2-server/src/main/java/mini/project/server/pms/handler/HelpCommand.java
+++ mini-project-2-server/src/main/java/mini/project/server/pms/handler/HelpCommand.java
@@ -10,17 +10,20 @@ public class HelpCommand implements Command {
   @Override
   public void execute(PrintWriter out, BufferedReader in) {
     try {
-    out.println("가이드 페이지입니다.\n");
+    out.println(" ");
+    out.println("[가이드 페이지]");
     out.println("/login => 로그인 페이지 진입");
     out.println("/test/start => 테스트 시작하기");
-    out.println("/test/result => 일반 : 자신의 테스트만 조회 가능 / 관리자 : 전체 회원의 테스트 결과 조회 가능");
+    out.println("/test/result => 일반 : 자신의 테스트 결과만 조회 가능");
+    out.println("/test/result => 관리자 : 전체 회원의 테스트 결과 조회 가능");
+    out.println(" ");
 
-    out.println("관리자 전용 가이드");
+    out.println("[관리자 가이드] ");
     out.println("/type/add => 성격유형 추가하기");
     out.println("/type/detail => 등록된 성격유형 상세정보 조회");
     out.println("/type/update => 등록된 성격유형 수정하기");
     out.println("/type/delete => 등록된 성격유형 삭제하기");
-
+    out.println(" ");
     out.println("나가시려면 아무 키나 눌러주세요.");
     Prompt.inputString(" ", out, in);
     } catch (Exception e) {
