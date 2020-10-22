3ab2f07c405fb5c36080c27d791ca90310eff945
seoggwonlee <sysktho@gmail.com>
2020-10-22 11:57:33 +0900

    .

--- mini-project-2-server/src/main/java/mini/project/server/pms/handler/Command.java
+++ mini-project-2-server/src/main/java/mini/project/server/pms/handler/Command.java
@@ -8,10 +8,3 @@ public interface Command {
     // 클라이언트가 보낸 데이터를 읽을 때 사용할 입력 스트림을 파라미터로 받는다.
     void execute(PrintWriter out, BufferedReader in);
 }
-package mini.project.server.pms.handler;
-
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-
-// 사용자의 명령을 처리하는 객체에 대해 호출할 메서드 규칙을 정의 한다.
-}
