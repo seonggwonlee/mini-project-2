917ee7018f3a7530bc618b474386b1ad64fad614
cgkim449 <cgkim449@gmail.com>
2020-10-23 10:20:19 +0900

    Thread sleep 추가

--- mini-project-2-server/src/main/java/mini/project/server/pms/handler/TestStartCommand.java
+++ mini-project-2-server/src/main/java/mini/project/server/pms/handler/TestStartCommand.java
@@ -29,49 +29,80 @@ public class TestStartCommand implements Command {
       out.println("[테스트 시작] 아무 키나 눌러주세요.");
       Prompt.inputString(" ", out, in);
 
+      int delay = 500;
       out.println("1. 다른 사람들에게 자신을 소개하는 것을 어려워 합니다.");
       test.setFirstAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
-      out.printf("%d번을 선택하셨습니다. 다음으로 넘어갑니다.", test.getFirstAnswer());
-
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
       out.println("2. 종종 주변을 무시하거나 잊어버리는 생각에 빠지곤 합니다.");
       test.setSecondAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("3. 이메일에 가능한 빨리 회신하려고 하고 지저분한 편지함을 참을 수 없습니다.");
       test.setThirdAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("4. 중암갑을 받을 때에도 쉽게 침착하게 집중력을 유지할 수 있습니다.");
       test.setFourthAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("5. 보통 대화를 먼저 시작하지 않습니다.");
       test.setFifthAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("2페이지로 넘어갑니다. (계속 하시려면 아무 키나 눌러주세요.)");
-          Prompt.inputString(" ", out, in);
+      Prompt.inputString(" ", out, in);
 
       out.println("6. 자신이 다른 사람들보다 뛰어나다고 생각합니다.");
       test.setFirstAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("7. 적응을 잘 하는 것 보다 체계적인 것이 더 중요합니다.");
       test.setSecondAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("8. 대게 의욕적이고 활동적입니다.");
       test.setThirdAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("9. 논쟁에서 이기는 것이 상대방을 불쾌하지 않도록 하는 것보다 더 중요합니다.");
       test.setFourthAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
 
       out.println("10. 종종 다른 사람들에게 자신을 정당화시켜야 할 것 만 같은 기분이 듭니다.");
       test.setFifthAnswer((Prompt.inputInt(
           "매우 그렇다(1), 그렇다(2), 보통이다(3), 아니다(4), 매우 아니다(5)", out, in)));
+      Thread.currentThread().sleep(delay);
+      out.println(" ");
+
+      //      out.print("검사 결과 집계 중");
+      //      out.println(" ");
+      //      Thread.currentThread().sleep(delay);
+      //      out.print(".");
+      //      out.println(" ");
+      //      Thread.currentThread().sleep(delay);
+      //      out.print(".");
+      //      out.println(" ");
+      //      Thread.currentThread().sleep(delay);
+      //      out.print(".");
+      //     out.println(" ");
 
       out.println("[테스트 결과]");
 
@@ -81,6 +112,7 @@ public class TestStartCommand implements Command {
       //          test.getFourthAnswer() + test.getFifthAnswer();
 
       out.printf("%s님의 검사결과입니다.\n", login.getName());
+      out.println(" ");
 
       if (test.getTotalScore() == 5) {
         Type type = findByNo(1);
@@ -88,34 +120,49 @@ public class TestStartCommand implements Command {
         out.printf("설명 : %s\n",type.getIntroduction());
         out.printf("장점 : %s\n",type.getStrength());
         out.printf("약점 : %s\n",type.getWeakness());
+        out.printf("연애 : %s\n",type.getRomance());
+        out.printf("우정 : %s\n",type.getFriendship());
       } else if (5 < test.getTotalScore() && test.getTotalScore() < 10) {
         Type type = findByNo(2);
         out.printf("유형 : %s\n",type.getName());
         out.printf("설명 : %s\n",type.getIntroduction());
         out.printf("장점 : %s\n",type.getStrength());
         out.printf("약점 : %s\n",type.getWeakness());
+        out.printf("연애 : %s\n",type.getRomance());
+        out.printf("우정 : %s\n",type.getFriendship());
       } else if (10 <= test.getTotalScore() && test.getTotalScore() < 15) {
         Type type = findByNo(3);
         out.printf("유형 : %s\n",type.getName());
         out.printf("설명 : %s\n",type.getIntroduction());
         out.printf("장점 : %s\n",type.getStrength());
         out.printf("약점 : %s\n",type.getWeakness());
+        out.printf("연애 : %s\n",type.getRomance());
+        out.printf("우정 : %s\n",type.getFriendship());
       } else if (15 <= test.getTotalScore() && test.getTotalScore() < 20) {
         Type type = findByNo(4);
         out.printf("유형 : %s\n",type.getName());
         out.printf("설명 : %s\n",type.getIntroduction());
         out.printf("장점 : %s\n",type.getStrength());
         out.printf("약점 : %s\n",type.getWeakness());
+        out.printf("연애 : %s\n",type.getRomance());
+        out.printf("우정 : %s\n",type.getFriendship());
+        out.printf("연애 : %s\n",type.getRomance());
+        out.printf("우정 : %s\n",type.getFriendship());
       } else if (20 <= test.getTotalScore() && test.getTotalScore() <= 25) {
         Type type = findByNo(5);
         out.printf("유형 : %s\n",type.getName());
         out.printf("설명 : %s\n",type.getIntroduction());
         out.printf("장점 : %s\n",type.getStrength());
         out.printf("약점 : %s\n",type.getWeakness());
+        out.printf("연애 : %s\n",type.getRomance());
+        out.printf("우정 : %s\n",type.getFriendship());
       } else {
       }
-      out.printf("%s님의 테스트 결과를 저장하였습니다.\n", login.getName());
       testList.add(test);
+      out.println(" ");
+      out.printf("%s님의 테스트 결과를 저장하였습니다.\n", login.getName());
+      out.println(" ");
+      out.flush();
 
     } catch (Exception e) {
       out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
